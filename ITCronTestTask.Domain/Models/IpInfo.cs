97e15b835770d99a1b35d4a904ee5146c3ee07aa
admin <enderfun3377@gmail.com>
2024-02-21 13:15:42 +0300

    init commit

---
+++ ITCronTestTask.Domain/Models/IpInfo.cs
@@ -0,0 +1,24 @@
+﻿namespace ITCronTestTask.Domain.Models;
+
+public class IpInfo
+{
+    public string Ip { get; set; } = string.Empty;
+
+    public string? Hostname { get; set; }
+
+    public string City { get; set; } = string.Empty;
+
+    public string Region { get; set; } = string.Empty;
+
+    public string Country { get; set; } = string.Empty;
+
+    public string Loc { get; set; } = string.Empty;
+
+    public string Org { get; set; } = string.Empty;
+
+    public string? Postal { get; set; }
+
+    public string Timezone { get; set; } = string.Empty;
+
+    public string Readme { get; set; } = string.Empty;
+}
